6d00fc34a36b51e98edb6c45f4dd0ea86c25c21c
vijay-igp <vijay.kumar@igp.com>
2017-06-09 18:09:07 +0530

    status update API integration

--- src/app/components/orders-action-tray/orders-action-tray.component.ts
+++ src/app/components/orders-action-tray/orders-action-tray.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { BackendService } from '../../services/backend.service';
 
 @Component({
@@ -12,11 +13,14 @@ export class OrdersActionTrayComponent implements OnInit {
   loadercount=[1,1];
   sidePanelDataLoading = true;
   orderByStatus;
+  orderUpdateByStatus;
+  orderUpdateByStatusDisable=false;
   orderId;
   private sidePanelData: Object;
 
   constructor(
-      private BackendService : BackendService
+      private BackendService : BackendService,
+      private router: Router,
       ) { }
 
   ngOnInit() {
@@ -26,6 +30,24 @@ export class OrdersActionTrayComponent implements OnInit {
     e.preventDefault();
     this.sidePanelData = null;
     this.orderByStatus = orderByStatus;
+    switch(orderByStatus){
+        case "Processed" :  this.orderUpdateByStatus = "Confirmed";
+                            this.orderUpdateByStatusDisable = false;
+            break;
+
+        case "Confirmed" :  this.orderUpdateByStatus = "OutForDelivery";
+                            this.orderUpdateByStatusDisable = false;
+            break;
+
+        case "OutForDelivery" : this.orderUpdateByStatus = "Shipped";
+                                this.orderUpdateByStatusDisable = false;
+            break;
+
+        case "Shipped" :    this.orderUpdateByStatus = "Shipped";
+                            this.orderUpdateByStatusDisable = true;
+            break;
+    }
+
     this.orderId = orderId;
     if(e.currentTarget.dataset.trayopen){
         console.log('close clicked ----->', this.trayOpen);
@@ -68,6 +90,48 @@ export class OrdersActionTrayComponent implements OnInit {
           console.log('sidePanel Response --->', response.result);
           _this.sidePanelData = Array.isArray(response.result) ? response.result : [response.result];
       });
+  }
+
+  updateOrderStatus(e, status, orderId){
+      let fkAssociateId = localStorage.getItem('fkAssociateId');
+      var _this = this;
+      var reqURL = "?responseType=json&scopeId=1&status="+status+"&fkAssociateId="+fkAssociateId+"&orderId="+orderId+"&method=igp.order.doUpdateOrderStatus";
+
+      let reqObj =  {
+          url : reqURL,
+          method : "post",
+          payload : {}
+      };
+
+      this.BackendService.makeAjax(reqObj, function(err, response, headers){
+          if(err) {
+              console.log(err)
+              return;
+          }
+          response = JSON.parse(response);
+          console.log('sidePanel Response --->', response.result);
+          //_this.router.navigate(['/dashboard-dfghj']);
+
+      });
+  }
+
+  printPage(e){
+      let printContents, popupWin;
+      printContents = document.getElementById('mainOrderSection').innerHTML;
+      popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+      popupWin.document.open();
+      popupWin.document.write(`
+          <html>
+              <head>
+                  <title>Order List</title>
+          <style>
+              //........Customized style.......
+              </style>
+          </head>
+          <body onload="window.print();window.close()">${printContents}</body>
+          </html>`
+      );
+      popupWin.document.close();
   }
 
 }
