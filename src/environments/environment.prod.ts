553081dbd1269c2fe53cefa9848fcb0b33150e2e
vijay-igp <vijay.kumar@igp.com>
2017-06-20 18:59:41 +0530

    Product Photos & component photos - config var on FE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   origin: 'http://34.204.124.92:8080/',
   apiInitial : 'IGPService/web/services/rest',
   productsURL : "https://d1xs5fw35mbn8b.cloudfront.net/p/m/",
-  productsCompURL : "dummy/"
+  productsCompURL : "assets/images/"
 };
