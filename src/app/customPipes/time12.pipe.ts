1866f43507034ebc3c722836f8a402bd065dde51
vijay-igp <vijay.kumar@igp.com>
2017-06-23 17:40:43 +0530

    misslaneous changes

---
+++ src/app/customPipes/time12.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'time12'
+})
+export class Time12Pipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+      if (!value) return value;
+
+      var time24 = value;
+      var startTime = time24.split('-')[0].trim();
+      var endTime = time24.split('-')[1].trim();
+
+      startTime = startTime.split(':')[0] > 12 ? (parseInt(startTime.split(':')[0]) - 12)+":00 PM" : startTime.split(':')[0]+":00 AM";
+      endTime = endTime.split(':')[0] > 12 ? (parseInt(endTime.split(':')[0]) - 12)+":00 PM" : endTime.split(':')[0]+":00 AM";
+
+      return startTime+" - "+endTime;
+  }
+
+}
