27cf0d55594ac84a81b4fa9b17e0cdb85cf305e4
vijay-igp <vijay.kumar@igp.com>
2017-06-26 19:00:05 +0530

    print template

--- src/app/components/orders-action-tray/orders-action-tray.component.ts
+++ src/app/components/orders-action-tray/orders-action-tray.component.ts
@@ -325,7 +325,7 @@ export class OrdersActionTrayComponent implements OnInit {
           <body onload="window.print();window.close()">${printContents}</body>
           </html>`
       );
-      popupWin.document.close();
+     // popupWin.document.close();
   }
 
   imagePreview(e, imgSrc){
