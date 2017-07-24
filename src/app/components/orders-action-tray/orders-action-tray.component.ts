a7b7e24d8fa364110e6f475bc9798b411345ecb5
vijay-igp <vijay.kumar@igp.com>
2017-07-24 15:44:10 +0530

    print css changes

--- src/app/components/orders-action-tray/orders-action-tray.component.ts
+++ src/app/components/orders-action-tray/orders-action-tray.component.ts
@@ -481,7 +481,7 @@ export class OrdersActionTrayComponent implements OnInit {
           <body onload="window.print();window.close()">${printContents}</body>
           </html>`
       );
-     popupWin.document.close();
+     //popupWin.document.close();
   }
 
   imagePreview(e, imgSrc){
