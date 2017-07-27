f6c9776034fc0f4dcc0a088a79860fab41816f17
vijay-igp <vijay.kumar@igp.com>
2017-07-27 20:13:11 +0530

    changed animation time

--- src/app/components/orders-action-tray/orders-action-tray.component.ts
+++ src/app/components/orders-action-tray/orders-action-tray.component.ts
@@ -17,7 +17,7 @@ import {environment} from "../../../environments/environment";
                 style({ height: '*', opacity: '1', transform: 'translateX(0)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'}),
                 sequence([
                     animate(".25s ease", style({ height: '*', opacity: '.2', transform: 'translateX(20px)', 'box-shadow': 'none'  })),
-                    animate(".1s ease", style({ height: '0', opacity: 0, transform: 'translateX(20px)', 'box-shadow': 'none'  }))
+                    animate("0.9s ease", style({ height: '0', opacity: 0, transform: 'translateX(20px)', 'box-shadow': 'none'  }))
                 ])
             ]),
             transition('void => active', [
