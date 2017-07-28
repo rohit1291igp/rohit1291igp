56558e6932deb23807ef09b5189ee31f63eb6567
vijay-igp <vijay.kumar@igp.com>
2017-07-28 15:09:42 +0530

    animation update

--- src/app/components/orders-action-tray/orders-action-tray.component.ts
+++ src/app/components/orders-action-tray/orders-action-tray.component.ts
@@ -16,15 +16,15 @@ import {environment} from "../../../environments/environment";
             transition('* => void', [
                 style({ height: '*', opacity: '1', transform: 'translateX(0)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'}),
                 sequence([
-                    animate(".4s ease", style({ height: '*', opacity: '.7', transform: 'translateX(99%)', 'box-shadow': 'none'  })),
-                    animate("0.9s ease", style({ height: '0', opacity: 0, transform: 'translateX(99%)', 'box-shadow': 'none'  }))
+                    animate(".4s ease", style({ height: '*', opacity: '.7', transform: 'translateX(5%)', 'box-shadow': 'none'  })),
+                    animate("0.9s ease", style({ height: '0', opacity: 0, transform: 'translateX(5%)', 'box-shadow': 'none'  }))
                 ])
             ]),
             transition('void => active', [
                 style({ height: '0', opacity: '0', transform: 'translateX(20px)', 'box-shadow': 'none' }),
                 sequence([
-                    animate(".1s ease", style({ height: '*', opacity: '.2', transform: 'translateX(20px)', 'box-shadow': 'none'  })),
-                    animate(".35s ease", style({ height: '*', opacity: 1, transform: 'translateX(0)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'  }))
+                    animate(".4s ease", style({ height: '*', opacity: '.2', transform: 'translateX(5%)', 'box-shadow': 'none'  })),
+                    animate(".9s ease", style({ height: '*', opacity: 1, transform: 'translateX(5%)', 'box-shadow': '0 1px 4px 0 rgba(0, 0, 0, 0.3)'  }))
                 ])
             ])
         ])
