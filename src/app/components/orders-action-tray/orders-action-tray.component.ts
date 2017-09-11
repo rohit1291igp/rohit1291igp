9d9a0d197818b3caac82b4fba7ebca3dba68e24b
vijay-igp <vijay.kumar@igp.com>
2017-09-11 13:29:46 +0530

    print

--- src/app/components/orders-action-tray/orders-action-tray.component.ts
+++ src/app/components/orders-action-tray/orders-action-tray.component.ts
@@ -542,7 +542,7 @@ export class OrdersActionTrayComponent implements OnInit {
           <body onload="window.print();window.close()">${printContents}</body>
           </html>`
       );
-     popupWin.document.close();
+     //popupWin.document.close();
   }
 
   imagePreview(e, imgSrc){
--- src/app/components/print-template/print-template.component.ts
+++ src/app/components/print-template/print-template.component.ts
@@ -13,7 +13,7 @@ export class PrintTemplateComponent implements OnInit {
   @Input('printType') printType : string;
     productsURL = environment.productsURL;
     productsCompURL = environment.productsCompURL;
-    messageBgImage = '/images/dummy.jpg';
+    messageBgImage = 'assets/images/Order-Sheet-Creative.png';
 
   constructor(
       public UtilityService: UtilityService,
