94376d97d09664f30fc88fba64d0942a2b83358a
vijay-igp <vijay.kumar@igp.com>
2017-09-18 13:31:49 +0530

    reports

---
+++ src/app/services/reports.service.ts
@@ -0,0 +1,166 @@
+import { Injectable } from '@angular/core';
+import {Http} from "@angular/http";
+import { BackendService } from './backend.service';
+import { UtilityService } from './utility.service';
+
+@Injectable()
+export class ReportsService {
+  /*searchFields:Array<any> = [
+      {
+          name:'orderNumber',
+          placeholder:'order Number'
+      },
+      {
+          name:'AWPNumber',
+          placeholder:'AWP Number'
+      },
+      {
+          name:'OrderPlaced',
+          placeholder:'Order Placed'
+      },
+      {
+          name:'OrderDispatched',
+          placeholder:'Order Dispatched'
+      }
+  ];
+  tableLabels:Array<any> = ['order Number', 'AWP Number', 'Order Placed', 'Order Dispatched', 'Actions', 'Status'];
+  tableData:Array<any> = [
+      {
+          ordernumber : 1152915,
+          awpnumber : 1152915,
+          orderPlaced : '23/07/17',
+          orderDispatched : '25/07/17',
+          action : 'Take Action',
+          status : "Delivered"
+      },
+      {
+          ordernumber : 1152915,
+          awpnumber : 1152915,
+          orderPlaced : '23/07/17',
+          orderDispatched : '25/07/17',
+          action : 'Take Action',
+          status : "Delivered"
+      },
+      {
+          ordernumber : 1152915,
+          awpnumber : 1152915,
+          orderPlaced : '23/07/17',
+          orderDispatched : '25/07/17',
+          action : 'Take Action',
+          status : "Delivered"
+      },
+      {
+          ordernumber : 1152915,
+          awpnumber : 1152915,
+          orderPlaced : '23/07/17',
+          orderDispatched : '25/07/17',
+          action : 'Take Action',
+          status : "Delivered"
+      },
+      {
+          ordernumber : 1152915,
+          awpnumber : 1152915,
+          orderPlaced : '23/07/17',
+          orderDispatched : '25/07/17',
+          action : 'Take Action',
+          status : "Delivered"
+      },
+      {
+          ordernumber : 1152915,
+          awpnumber : 1152915,
+          orderPlaced : '23/07/17',
+          orderDispatched : '25/07/17',
+          action : 'Take Action',
+          status : "Delivered"
+      },
+      {
+          ordernumber : 1152915,
+          awpnumber : 1152915,
+          orderPlaced : '23/07/17',
+          orderDispatched : '25/07/17',
+          action : 'Take Action',
+          status : "Delivered"
+      },
+      {
+          ordernumber : 1152915,
+          awpnumber : 1152915,
+          orderPlaced : '23/07/17',
+          orderDispatched : '25/07/17',
+          action : 'Take Action',
+          status : "Delivered"
+      }
+
+  ];*/
+
+    //https://jsonblob.com/9fb80aee-9a1c-11e7-aa97-275b22669468
+    //http://www.mocky.io/v2/59bbdd060f0000c202ff878b
+    _reportData ={
+                    "searchFields" : [
+                        {
+                            "name" : "orderNumber",
+                            "type" : "number",
+                            "placeholder" : "order Number"
+                        },
+                        {
+                            "name" : "AWPNumber",
+                            "type" : "number",
+                            "placeholder" : "AWP Number"
+                        },
+                        {
+                            "name" : "OrderPlacedon",
+                            "type" : "date",
+                            "placeholder" : "Select Delivery Date"
+                        },
+                        {
+                            "name" : "OrderDispatchedOn",
+                            "type" : "date",
+                            "placeholder" : "Select Dispatch Date"
+                        }
+                    ],
+                    "summary" : [
+                        {
+                            "label" : "Total orders",
+                            "icon" : "",
+                            "value" : "20"
+                        },
+                        {
+                            "label" : "Total Amount",
+                            "icon" : "",
+                            "value" : "4500000"
+                        }
+                    ],
+                    "tableHeaders" : ["order Number", "AWP Number", "Order Placed", "Order Dispatched", "Actions", "Status"],
+                    "tableData" : [
+                            [1152915, 1152935, "23/07/17", "25/07/17", ["Edit", "Delete"], "Delivered"],
+                            [1152915, 1152935, "23/07/17", "25/07/17", ["Edit", "Delete"], "Delivered"],
+                            [1152915, 1152935, "23/07/17", "25/07/17", ["Edit", "Delete"], "Delivered"],
+                            [1152915, 1152935, "23/07/17", "25/07/17", ["Edit", "Delete"], "Delivered"],
+                            [1152915, 1152935, "23/07/17", "25/07/17", ["Edit", "Delete"], "Delivered"]
+                    ]
+                };
+  constructor(
+      private http: Http,
+      private BackendService: BackendService,
+      private UtilityService: UtilityService
+      ) { }
+
+  getReportData(reportType, cb){
+      var _this = this;
+      if(!cb){
+          return _this._reportData;
+      }
+
+
+      /* make API call
+        a.tableData
+        b.deduce table label from tableData
+        c.if required change search fields - depending on - report type
+       */
+
+
+  }
+
+}
+
+
+
