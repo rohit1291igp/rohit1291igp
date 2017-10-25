2864d82842bdd715eb7b3442aacce8dc2a5c6c86
vijay-igp <vijay.kumar@igp.com>
2017-10-25 13:15:56 +0530

    sla time

--- src/app/customPipes/time12.pipe.ts
+++ src/app/customPipes/time12.pipe.ts
@@ -8,14 +8,21 @@ export class Time12Pipe implements PipeTransform {
   transform(value) {
       if (!value) return value;
 
-      var time24 = value;
-      var startTime = time24.split('-')[0].trim();
-      var endTime = time24.split('-')[1].trim();
+      if(value.indexOf('-') !== -1){
+          var time24 = value;
+          var startTime = time24.split('-')[0].trim();
+          var endTime = time24.split('-')[1].trim();
 
-      startTime = startTime.split(':')[0] > 12 ? (parseInt(startTime.split(':')[0]) - 12)+":"+startTime.split(':')[1].replace("hrs", "").trim()+" PM" : startTime.split(':')[0]+":"+startTime.split(':')[1].replace("hrs", "").trim()+" AM";
-      endTime = endTime.split(':')[0] > 12 ? (parseInt(endTime.split(':')[0]) - 12)+":"+endTime.split(':')[1].replace("hrs", "").trim()+" PM" : endTime.split(':')[0]+":"+endTime.split(':')[1].replace("hrs", "").trim()+" AM";
+          startTime = startTime.split(':')[0] > 12 ? (parseInt(startTime.split(':')[0]) - 12)+":"+startTime.split(':')[1].replace("hrs", "").trim()+" PM" : startTime.split(':')[0]+":"+startTime.split(':')[1].replace("hrs", "").trim()+" AM";
+          endTime = endTime.split(':')[0] > 12 ? (parseInt(endTime.split(':')[0]) - 12)+":"+endTime.split(':')[1].replace("hrs", "").trim()+" PM" : endTime.split(':')[0]+":"+endTime.split(':')[1].replace("hrs", "").trim()+" AM";
 
-      return startTime+" - "+endTime;
+          return startTime+" - "+endTime;
+      }else{
+          var startTime = value.trim();
+          startTime = startTime.split(':')[0] > 12 ? (parseInt(startTime.split(':')[0]) - 12)+":"+startTime.split(':')[1].replace("hrs", "").trim()+" PM" : startTime.split(':')[0]+":"+startTime.split(':')[1].replace("hrs", "").trim()+" AM";
+          return startTime;;
+      }
+
   }
 
 }
