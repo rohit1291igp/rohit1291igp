be0a3c3786aaf0a483e1296ad6cfda854ea31e2b
vijay-igp <vijay.kumar@igp.com>
2017-11-29 17:21:33 +0530

    order instruction line break

---
+++ src/app/components/feeds/feeds.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
+import { UtilityService } from '../../services/utility.service';
+import {environment} from "../../../environments/environment";
+
+@Component({
+  selector: 'app-feeds',
+  templateUrl: './feeds.component.html',
+  styleUrls: ['./feeds.component.css']
+})
+export class FeedsComponent implements OnInit {
+  _feeds=[
+      {
+          "orderId": "1004300",
+          "instruction": "Kindly add a msg - \"Wish you a very Happy Birthday Amit.\"\r\n\r\nAnd kindly arrange for a delivery anytime between 3 to 7"
+      },
+      {
+          "orderId": "1004214",
+          "instruction": "Please arrange flowers in glass vase"
+      },
+
+      {
+          "orderId": "1005323",
+          "instruction": " mention that on the cake \r\nalso mentioned the message to the recipient:Happy Marriage Anniversary may you have many more to come."
+      },
+      {
+          "orderId": "1004855",
+          "instruction": "The recipient address is changed in this order.\r\n\r\nMr. K. Vijay 30/1, 1st Floor, Royal City Complex, Karur Bye Pass Road, Near Chatram Bus Stand, V.N.Nagar, Trichy Tiruchirappalli, Tamil Nadu 620002 India +91-8148002622"
+      },
+      {
+          "orderId": "1005323",
+          "instruction": "Please make a note of the address change.\r\n\r\nOrder id#:1005323\r\n\r\nMr. sathish \r\n8/653 kerada street, nanjanad village post , Ooty - 643004 \r\nTamil Nadu, India\r\nTele Phone :- 9600080991- - , karthikvsv@gmail.com \r\nCell Phone :- 9600080991"
+      }
+  ];
+
+    isMobile=environment.isMobile;
+    public feedData: any=[
+      {
+          "orderId": "",
+          "instruction": ""
+      },
+        {
+            "orderId": "",
+            "instruction": ""
+        },
+        {
+            "orderId": "",
+            "instruction": ""
+        },
+        {
+            "orderId": "",
+            "instruction": ""
+        },
+        {
+            "orderId": "",
+            "instruction": ""
+        }
+
+  ];
+  constructor(
+      public BackendService : BackendService,
+      public UtilityService: UtilityService
+  ) { }
+
+  ngOnInit() {
+    this.getFeeds();
+  }
+
+  getFeeds(){
+      var _this=this;
+      var reqObj={
+          url:'vendorinstructionfeed?fkAssociateId='+localStorage.getItem('fkAssociateId'),
+          method:'get'
+      };
+
+      _this.BackendService.makeAjax(reqObj, function(err, response, headers){
+          if(err || JSON.parse(response).error) {
+              console.log('Error=============>', err, JSON.parse(response).errorCode);
+          }
+          console.log('feeds Response --->', JSON.parse(response).result);
+          _this.feedData=JSON.parse(response).result;
+      });
+  }
+
+}
