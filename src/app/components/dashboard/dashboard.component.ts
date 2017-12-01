5488e1ca9d525a5064822789ecc0766b6249a5af
vijay-igp <vijay.kumar@igp.com>
2017-12-01 15:33:40 +0530

    changed dateChanged event with ngModelChange

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -113,6 +113,10 @@ export class DashboardComponent implements OnInit {
     console.log('Side-panel opened for status: ', status);
   }
 
+    onDateChanged1(event) {
+        console.log('onDateChanged1 called ----------');
+    }
+
   onDateChanged(event: IMyDateModel) {
     console.log('Date changed');
     console.log('onDateChanged(): ', event.date, ' - jsdate: ', new Date(event.jsdate).toLocaleDateString(), ' - formatted: ', event.formatted, ' - epoc timestamp: ', event.epoc);
