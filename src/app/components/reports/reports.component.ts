a9483fa9a332fddf933c35256a922375c5f270b4
jailany-igp <abdul.jailany@igp.com>
2018-04-11 14:07:31 +0530

    created vendor list for bulk assign

--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -315,6 +315,7 @@ export class ReportsComponent implements OnInit{
 
   bulkAssignAction(){
       var _this = this;
+    
       console.log(_this.assignedVendors);
       if(Object.keys(_this.assignedVendors).length > 0){
         let reqObj =  {
