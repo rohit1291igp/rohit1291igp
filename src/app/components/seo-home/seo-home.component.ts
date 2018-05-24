1dbf2f1567c9e94d9839d9484fc266f71a611474
jailany-igp <abdul.jailany@igp.com>
2018-05-24 15:18:04 +0530

    API integration for updating meta home information

--- src/app/components/seo-home/seo-home.component.ts
+++ src/app/components/seo-home/seo-home.component.ts
@@ -26,19 +26,55 @@ export class SeoHomeComponent implements OnInit {
     saveSeoData() {
         const data = {};
         data['fkasid'] = this.model.webstore;
-        data['title'] = this.model.title;
-        data['metaTitle'] = this.model.metaTitle;
-        data['metaDesc'] = this.model.metaDesc;
-        data['metaKeywords'] = this.model.metaKeywords;
+        data['seotitle'] = this.model.title;
+        data['seodescription'] = this.model.metaDesc;
+        data['seokeywords'] = this.model.metaKeywords;
 
         console.log(data);
 
         // get the endpoint from API team, code the logic for saving data and refreshing the page
+        const _this = this;
+        const reqObj = {
+            url: 'blogs/updatemetahome',
+            payload: data,
+            method: 'put'
+        };
+
+        _this.BackendService.makeAjax(reqObj, function(err, response, headers){
+            if (err || response.error) {
+                console.log('Error=============>', err, response.errorCode);
+                alert('There was an error while creating the article');
+            }
+            alert('The meta information has been updated successfully.');
+        });
 
     }
 
-    getSeoData() {
+    getMetaDataOnStoreChange() {
+        this.getSeoData((data) => {
+            this.model.title = data.seotitle;
+            this.model.metaTitle = data.seotitle;
+            this.model.metaKeywords = data.seokeywords;
+            this.model.metaDesc = data.seodescription;
+        });
+    }
+
+    getSeoData(cb) {
         // get the endpoint from API team, code the logic for fetching data
+        const _this = this;
+        const fkasid = this.model.webstore;
+        const reqObj = {
+            url: `blogs/getmetahome?fkasid=${fkasid}`,
+            method: 'get'
+        };
+
+        _this.BackendService.makeAjax(reqObj, function(err, response, headers){
+            if (err || response.error) {
+                console.log('Error=============>', err, response.errorCode);
+                alert('There was an error while creating the article');
+            }
+            cb(response.data);
+        });
     }
 
 }
