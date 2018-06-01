a7a09e355976a242d97e817aa7e570d6ddb9b2dd
Chetan Barot <cbarot-IGP>
2018-06-01 13:12:00 +0530

    Added template for header

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -159,7 +159,7 @@ export class CategoryComponent implements OnInit {
     $('body')[0].style.overflow = 'auto';
     this.testAnimate = 'void';
     $('#target :input').prop('disabled', false);
-    if (event.data !== undefined) {
+    if (event.data !== undefined && event.data !== 'Not') {
     this.categories = event.data;
     }
   };
