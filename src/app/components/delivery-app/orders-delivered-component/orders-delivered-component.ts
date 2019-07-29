356f37c5f5d730826487d278409254f7e4530694
sandipjaiswar-igp <sandip.jaiswar@indiangiftsportal.com>
2019-07-29 13:27:21 +0530

    fix typing bug

--- src/app/components/delivery-app/orders-delivered-component/orders-delivered-component.ts
+++ src/app/components/delivery-app/orders-delivered-component/orders-delivered-component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class OrdersDeliveredComponent implements OnInit {
 
-    orders = [];
+    orders:any;
     public orderId:number;
     fkAssociateId: string;
     fkUserId: string;
