426f9cbac7f7da8d3b23afa9bf2236c05ccb70ec
sandipjaiswar-igp <sandip.jaiswar@indiangiftsportal.com>
2019-11-15 10:15:01 +0530

    added voucher model component in voucher module and filter pipe

--- src/app/modules/voucher.module.ts
+++ src/app/modules/voucher.module.ts
@@ -1,32 +1,77 @@
+import { CdkTableModule } from '@angular/cdk/table';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MatButtonModule, MatCardModule, MatDatepickerModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatPaginatorModule, MatRadioModule, MatSelectModule, MatSnackBarModule, MatSortModule, MatTableModule } from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
+import { CouponComponent } from 'app/components/coupon/coupon.component';
+import { ApiService } from 'app/services/api.service';
 import { MyDatePickerModule } from 'mydatepicker';
-import { VoucherComponent } from '.././components/voucher/voucher.component';
-import { VoucherModelComponent } from '.././components/voucher-model/voucher-model.component';
+import { HeaderInterceptor } from '../others/header-request.interceptor';
+import { ResponseInterceptor } from '../others/api-response.interceptor';
+import { GiftsVoucherComponent, GiftVoucherDialog } from 'app/components/gifts-voucher/gifts-voucher.component';
+import { VoucherModelComponent } from 'app/components/voucher-model/voucher-model.component';
+import { VoucherComponent } from 'app/components/voucher/voucher.component';
 import { FilterPipeModule } from 'app/customPipes/filter.pipe';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: VoucherComponent
-  }
+    {
+        path: 'voucher',
+        component: CouponComponent
+    },
+    {
+        path: 'gifts-voucher',
+        component: GiftsVoucherComponent
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    FormsModule,
-    ReactiveFormsModule,
-    MyDatePickerModule,
-    FilterPipeModule
-  ],
-  declarations: [
-    VoucherComponent,
-    VoucherModelComponent
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        FormsModule,
+        HttpModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        MyDatePickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTableModule,
+        MatCardModule,
+        MatButtonModule,
+        MatMenuModule,
+        MatDialogModule,
+        MatSnackBarModule,
+        CdkTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatDatepickerModule,
+        MatRadioModule,
+        MatSelectModule,
+        FilterPipeModule
+    ],
+    declarations: [
+        CouponComponent,
+        GiftsVoucherComponent,
+        GiftVoucherDialog,
+        VoucherComponent,
+        VoucherModelComponent
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: HeaderInterceptor,
+            multi: true
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ResponseInterceptor,
+            multi: true
+        },
+        ApiService],
+        entryComponents:[GiftVoucherDialog]
 })
 export class VoucherModule { }
