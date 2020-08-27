06d213e3700aee8f9bffa079ef1dcbc49cdcda39
Parvez Shaikh <parvez.shaikh@gmail.com>
2020-08-27 17:57:00 +0530

    uploaded-image report and offer panel fix

--- src/app/components/offer-page-management/offer-page-management.component.ts
+++ src/app/components/offer-page-management/offer-page-management.component.ts
@@ -53,7 +53,7 @@ export class OfferPageManagementComponent implements OnInit, AfterViewChecked {
       "coupon_code": ['', Validators.required],
       "webstore": ['', Validators.required],
       "expiry_date": ['', Validators.required],
-      "redirect_url": ['', Validators.required],
+      "redirect_url": [''],
       "discount": ['', Validators.required],
       "coupon_type": ['value_based', Validators.required],
       "desc": ['', Validators.required],
