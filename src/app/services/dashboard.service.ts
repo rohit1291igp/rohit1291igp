13e87ba270ef77f4fc9109758a129987dd72cf36
sandipjaiswar-igp <sandip.jaiswar@indiangiftsportal.com>
2020-09-18 19:39:09 +0530

    new dashboard redirection issue

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -69,6 +69,9 @@ export class DashboardService {
             if(userType == 'microsite' || userType == 'microsite-zeapl'){
                 this.router.navigate(['/dashboard-microsite']);
             }
+            if(userType == 'warehouse'){
+                this.router.navigate(['/new-dashboard']);
+            }
             // if(userType == 'warehouse'){
             //     this.router.navigate(['/sendemail']);
             // }
