35ebb5e63659aef548dabbf608b1fd610049ccfa
Parvez Shaikh <parvez.shaikh@gmail.com>
2020-11-03 19:23:52 +0530

    decentralisation issue fixed

--- src/app/components/product-decentralization/delivery-priority/delivery-priority.component.ts
+++ src/app/components/product-decentralization/delivery-priority/delivery-priority.component.ts
@@ -108,7 +108,7 @@ export class DeliveryPriorityComponent implements OnInit,AfterViewChecked {
       payload=reqData;
     }
     let startLimit=_this.dataSource.data.length||0;
-    let endLimit=startLimit+100;
+    let endLimit=100;
     const reqObj = {
       url: `warehouse/decentralized/getDeliveryPriorityList?startLimit=${startLimit}&endLimit=${endLimit}${source}`,
       method: 'post',
@@ -130,7 +130,7 @@ export class DeliveryPriorityComponent implements OnInit,AfterViewChecked {
             });
             (<FormArray>_this.tableform.get("tableEntries")).push(control);
           });
-        _this.dataSource = new MatTableDataSource(tableData);
+        _this.dataSource.data=_this.dataSource.data.concat(tableData)
         _this.tableHeaders = response['tableHeaders']
         _this.tableHeaders.splice(_this.tableHeaders.indexOf("Id"),1)
         _this.tableHeaders.unshift('select')
