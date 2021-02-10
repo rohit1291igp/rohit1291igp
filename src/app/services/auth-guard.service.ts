f0ede8600089e3e2b706f4033eb77b1b1b3cbbae
sandip.jaiswar@indiangiftsportal.com <sandip.jaiswar@indiangiftsportal.com>
2021-02-10 19:46:31 +0530

    navigation optimisation

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, RoutesRecognized } from '@angular/router';
 import { CookieService } from './cookie.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-
-    constructor(private router: Router,private cookieService: CookieService) { }
+previousUrl:string;
+    constructor(private router: Router,private cookieService: CookieService) {
+        this.router.events
+        .filter(e => e instanceof RoutesRecognized)
+        .pairwise()
+        .subscribe((event: any[]) => {
+        console.log(event[0].urlAfterRedirects, 'precviuiosurl');
+        this.previousUrl = event[1].urlAfterRedirects;
+        });
+     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         if (this.cookieService.getCookie('currentUserToken') || localStorage.getItem('currentUser')) {
             // logged in so return true
             return true;
         }
-
+        sessionStorage.setItem('previousUrl', location.href.split("#")[1])
         // not logged in so redirect to login page with the return url
         //this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
         this.router.navigate(['/login']);
