2f5ac3c4d51f3115065df51aa2187f790e5351cf
rohit1291igp <rohit.patodia@igp.com>
2021-04-05 10:32:42 +0530

    convert maticon

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, HostListener, DoCheck } from '@angular/core';
   selector: 'app-footer',
   template: `<div class="row footer">
   <div class="col-md-6 col-md-offset-3 underline"> <div class="d-flex justify-content-center align-items-center auto-margin width-fit-content" *ngIf="whitelabelStyle else regularFooter"><span style="margin-right: 7px;">Powered by: </span><img src="https://cdn.igp.com/f_auto,q_auto/banners/IGP-for-business-48_new.png?v=3" width="100" height="100" class="image" /></div>
-                <ng-template #regularFooter>Copyright &copy; 2019-2020. IGP.com. All rights reserved</ng-template>
+                <ng-template #regularFooter>Copyright &copy; 2020-2021. IGP.com. All rights reserved</ng-template>
                 </div>
             </div>`,
   styles: [`.footer{
